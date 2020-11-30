d333a1c1ca0903186af9ba6f4d0525c25fff9e97
양미루 <david@yangmiluui-MacBookPro.local>
2020-11-30 16:58:46 +0900

    Create express server and Install babel

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hi!");
+import express from "express"
+const app = express()
+
+const PORT = 3000
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`)
+
+const handleHome = (req, res) => res.send('Hello WTSK!')
+
+const handleProfile = (req, res) => res.send('Hello Profile')
+
+app.get('/', handleHome)
+app.get('/profile', handleProfile)
+
+app.listen(PORT, handleListening)
