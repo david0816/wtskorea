ab327926874780efe70e956ed12a2e8a6d3483d2
양미루 <didalfn88@gmail.com>
2021-05-18 15:44:14 +0900

    gh-pages

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan"
 import helmet from "helmet"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
+import path from "path"
 import routes from "./routes"
 import globalRouter from "./routers/globalRouter"
 import userRouter from "./routers/userRouter"
@@ -18,13 +19,14 @@ const app = express()
 
 app.use(helmet())
 app.set("view engine","pug")
+app.set("views", path.join(__dirname, "views"))
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan("dev"))
 
 app.use(express.static(__dirname+"/images"))
-app.use("/static", express.static("static"))
+app.use("/static", express.static(path.join(__dirname, "static")))
 
 app.use(localsMiddleware)
 
