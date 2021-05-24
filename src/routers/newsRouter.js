953211510bb2fc601bc7168bfbc9e24f4d994d21
양미루 <didalfn88@gmail.com>
2021-05-24 16:34:34 +0900

    update

--- src/routers/newsRouter.js
+++ src/routers/newsRouter.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { news, newsLetter, newsStory } from "../controllers/pageController"
+import { news, newsLetter, newsNotice, newsStory } from "../controllers/pageController"
 import routes from "../routes"
 
 const newsRouter = express.Router()
 
 newsRouter.get("/", news)
+newsRouter.get(routes.newsNotice, newsNotice)
 newsRouter.get(routes.newsStory, newsStory)
 newsRouter.get(routes.newsLetter, newsLetter)
 
